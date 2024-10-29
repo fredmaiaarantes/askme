77fd9e0b7d8e5da7034a1ab5f1b8da0ba6bce0bd
Frederico Maia <fredmaiaarantes@gmail.com>
2024-10-29 18:20:01 -0300

    add schema

--- imports/api/questions/questions.js
+++ imports/api/questions/questions.js
@@ -1,3 +1,15 @@
 import { Mongo } from "meteor/mongo";
 
 export const Questions = new Mongo.Collection("questions");
+
+const schema = {
+  _id: String,
+  userId: String,
+  userAvatarUrl: String,
+  userName: String,
+  description: String,
+  answered: Boolean,
+  createdAt: Date,
+};
+
+Questions.attachSchema(schema);
