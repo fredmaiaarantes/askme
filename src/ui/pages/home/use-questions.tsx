5ceccc0046a215a37dac938227c28b02af9f786b
Frederico Maia <fredmaiaarantes@gmail.com>
2024-11-29 18:33:53 -0300

    using grubba-rpc

--- src/ui/pages/home/use-questions.tsx
+++
@@ -1,9 +0,0 @@
-import { api } from '../../api';
-
-export const useQuestions = () => {
-  const {
-    data: allQuestions,
-  } = api.questions.allQuestions.usePublication();
-
-  return { allQuestions };
-};
